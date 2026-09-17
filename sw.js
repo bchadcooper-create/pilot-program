1a597d8e257fb4fec8da8aced60e74ac629cddba
bchadcooper-create <b.chad.cooper@gmail.com>
2026-09-16 19:45:02 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Flight Crew Fitness — Service Worker
-// Version: 5.42.0
-const CACHE = 'fcf-v5-42-0';
+// Version: 5.42.3
+const CACHE = 'fcf-v5.42.3';
 const CORE = [
   './',
   './index.html',
